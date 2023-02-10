ac26224039413249cb9472f583f33208d8a02329
Eimhin Heenan-Roberts <heenanre@tcd.ie>
2023-02-10 11:38:02 +0000

    testing the gettting to a webpage

--- tests/backend.spec.js
+++ tests/backend.spec.js
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
+const AxeCore = require()
 
 test('test', async ({ page }) => {
   await page.goto('https://www.google.com/');
 });
 
 test.afterAll(async () => {
-    console.log('Done with tests');
+    
 });

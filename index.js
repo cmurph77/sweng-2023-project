e31aa431294144d46d1b8c110346cd4db846215a
Mark Connor <maconnor@tcd.ie>
2023-02-22 12:21:28 +0000

    Working api with demo website

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ app.use(express.json({ limit: '1mb' }));
 app.post('/api', (request, response) => {
     const url = request.body;
     console.log(url);   
-    
+    const execSync = require('child_process').execSync;
+    const output = execSync('npx playwright test /backend', { encoding: 'utf-8' });
+    console.log(output);
     response.json({
-        data: 'axe-core results'
+        data: output
     })
     
 })

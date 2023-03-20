f7f054abc36cb310fbb72bb15c698ae33a58acbe
Eimhin Heenan-Roberts <heenanre@tcd.ie>
2023-03-20 12:51:24 +0000

    fixed how json is outputed and formatted

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { validateHeaderValue } = require('http');
 
 const {MongoClient} = require('mongodb');
 
+var ObjectID = require('mongodb').ObjectId;
+
 const app = express();
 app.listen(3000);
 app.use(express.static('html'));
@@ -26,45 +28,79 @@ app.post('/api', (request, response) => {
     var descriptions = [];
     var helps = [];
     var helpUrls = [];
+
+    var keepDataTogether = [];
+
     console.log("before for each");
-   browsers.forEach(browser => {
-        if (browser.indexOf("inapplicable: [") != -1) {
-            firstPos = browser.indexOf("inapplicable: [");
-            jsonItems = browser.substring(firstPos++).split("{\n");
-            jsonItems.forEach(element => {
-                if (element.search("id") != -1) {
-                    //creating ids array
-                    var currentVar = element.substring(element.search("id: "),element.search("impact")); 
-                    if (currentVar.search("id: ")!=-1) ids.push(currentVar); 
+    browsers.forEach(browser => {
+            if (browser.indexOf("inapplicable: [") != -1) {
+                firstPos = browser.indexOf("inapplicable: [");
+                jsonItems = browser.substring(firstPos++).split("{\n");
+                jsonItems.forEach(element => {
+                    if (element.search("id") != -1) {
+                        //creating ids array
+                        var currentVar = element.substring(element.search("id: "),element.search("impact")); 
+                        if (currentVar.search("id: ")!=-1) ids.push(currentVar); 
 
-                    //creating impacts array
-                    var currentVar = element.substring(element.search("impact: "),element.search("tags")); 
-                    if (currentVar.search("impact: ")!=-1) impacts.push(currentVar); 
+                        //creating impacts array
+                        var currentVar = element.substring(element.search("impact: "),element.search("tags")); 
+                        if (currentVar.search("impact: ")!=-1) impacts.push(currentVar); 
 
-                    //creating descriptions array
-                    var currentVar = element.substring(element.search("description: "),element.search("help")); 
-                    if (currentVar.search("description: ")!=-1) descriptions.push(currentVar); 
+                        //creating descriptions array
+                        var currentVar = element.substring(element.search("description: "),element.search("help")); 
+                        if (currentVar.search("description: ")!=-1) descriptions.push(currentVar); 
 
-                    //creating helps array
-                    var currentVar = element.substring(element.search("help: "),element.search("helpUrl")); 
-                    if (currentVar.search("help: ")!=-1) helps.push(currentVar); 
+                        //creating helps array
+                        var currentVar = element.substring(element.search("help: "),element.search("helpUrl")); 
+                        if (currentVar.search("help: ")!=-1) helps.push(currentVar); 
 
-                    //creating helpUrls array
-                    var currentVar = element.substring(element.search("helpUrl: "),element.search("nodes")); 
-                    if (currentVar.search("helpUrl: ")!=-1) helpUrls.push(currentVar); 
-                }
-            });
-        }
-    });
+                        //creating helpUrls array
+                        var currentVar = element.substring(element.search("helpUrl: "),element.search("nodes")); 
+                        if (currentVar.search("helpUrl: ")!=-1) helpUrls.push(currentVar); 
+                    }
+                    let id = ids.pop();
+                    let temp;
+                    if (id != null) {
+                        temp = id.split("'");
+                        id = temp[1];
+                    }
+                    let impact = impacts.pop();
+                    if (impact != null) {
+                        temp = impact.split("'");
+                        impact = temp[1];
+                    }
+                    let description = descriptions.pop();
+                    if (description != null) {
+                        temp = description.split("'");
+                        description = temp[1];
+                    }
+                    let help = helps.pop();
+                    if (help != null) {
+                        temp = help.split("'");
+                        help = temp[1];
+                    }
+                    let url = helpUrls.pop();
+                    if (url != null) {
+                        temp = url.split("'");
+                        url = temp[1];
+                    }
+                    const finalData = {
+                        id: id,
+                        impactList: impact,
+                        description: description,
+                        help: help,
+                        helpURL: url
+                    }
+                    keepDataTogether.push(finalData);
+                });
+            }
+        });
     const data = {      // this is the axe core data organised in JSON format
-        idList: ids,
-        impactList: impacts,
-        descriptionList: descriptions,
-        helpList: helps,
-        helpUrlList: helpUrls
+        problems: keepDataTogether
     }
 
-    create(client, data);
+    let uniqueID = create(client, data);
+    console.log(uniqueID);
 
     response.json({
         data: dataStream  // sends back the results in json format
@@ -73,6 +109,8 @@ app.post('/api', (request, response) => {
 
 
 function create(client, newTest) {
-    client.db("backend_sweng").collection("axe_core_results").insertOne(newTest);
+    let id = new ObjectID(32);
+    client.db("backend_sweng").collection("axe_core_results").insertOne({_id: id, newTest});
+    return id;
 }
 

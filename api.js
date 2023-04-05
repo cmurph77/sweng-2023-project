054a17a193bb2aff1316bb4ae1eb0ff3bea0dfd5
ohse095 <124053168+ohse095@users.noreply.github.com>
2023-04-05 17:10:41 +0100

    Add files via upload

---
+++ api.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+// Create an instance of the Express app
+const app = express();
+
+// Create a router to handle routes
+const router = express.Router();
+
+// Define a route that responds with a JSON object when a GET request is made to the root path
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
+});
+
+// Use the router to handle requests to the `/.netlify/functions/api` path
+app.use(`/.netlify/functions/api`, router);
+
+// Export the app and the serverless function
+module.exports = app;
+module.exports.handler = serverless(app);

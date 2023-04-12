b77ecfc81e304b1a2678781ea78622675bab369f
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-04-12 14:28:59 +0100

    delted api code - only frontend left

--- tests/navToBB.spec.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/');
-
-});
--- tests/open.google.spec.js
+++
@@ -1,7 +0,0 @@
-const{test, expect} = require('@playwright/test');
-
-test('basic test', async ({page}) => {
-    await page.goto('https://google.com');
-    const name = await page.innerText('title');
-    expect(name).toBe('Google');
-});
